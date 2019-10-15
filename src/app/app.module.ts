d7764abd8f9fb4402ddf2a1c4e7f1711aabc3e5d
Piotr Zuzak <piotr.zuzak@hotmail.com>
2019-10-15 21:42:04 +0200

    Add Firebase and Angular Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,17 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
