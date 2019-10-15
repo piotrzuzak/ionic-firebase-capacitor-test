4c3d4a590a59b9518826b3d67c4db013ab67eee3
Piotr Zuzak <piotr.zuzak@hotmail.com>
2019-10-15 21:27:28 +0200

    Add Goole Login Button

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+import { cfaSignIn, mapUserToUserInfo } from 'capacitor-firebase-auth';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,5 +9,25 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+
+  doGoogleLogin(): void {
+      console.log('google login button clicked');
+      try {
+        cfaSignIn('google.com')
+        .pipe(
+          mapUserToUserInfo(),
+        ).subscribe(
+          (user: any) => {
+            console.log(user.displayName);
+            console.log('[subcribe-success] => google login end');
+          },
+          err => {
+            console.log('[subcribe-success] => google login end: ' + err);
+          }
+        );
+      } catch (exception) {
+        console.log('Exception found: ' + exception);
+      }
+  }
 
 }
